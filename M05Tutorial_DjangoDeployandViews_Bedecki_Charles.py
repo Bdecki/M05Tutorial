44dffdea3d59c100d8fd6b5b84b151a2484e5b1c
Bdecki <112962331+Bdecki@users.noreply.github.com>
2022-11-01 11:36:06 -0400

    Add files via upload

---
+++ M05Tutorial_DjangoDeployandViews_Bedecki_Charles.py
@@ -0,0 +1,5 @@
+## M05 Tutorial - Django Deploy and Views
+## Charles Bedecki
+## 11/1/2022
+## The purpose of this program is to begin learning about full-stack Python development utilizing Django.
+
